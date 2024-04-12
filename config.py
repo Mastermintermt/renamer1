f4b1cc71d8d0eb6b6af5b82af7745808972f8bf4
Mastermintermt <150274518+Mastermintermt@users.noreply.github.com>
2024-04-12 15:21:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "20919286"))
 API_HASH = environ.get("API_HASH", "57b85f72104db3f08f9795b0410eb556")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7101381082:AAGQQrFIDpGgSyi_voDJ-JoTv4hRIPmwRo0")
-ADMIN = int(environ.get("ADMIN", "5909932224"))          
+ADMIN = int(environ.get("ADMIN", "6976445947"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
