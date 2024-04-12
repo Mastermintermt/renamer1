2c948a4a7d5ce7dda5323a0a207bdd6329d9d8f8
Mastermintermt <150274518+Mastermintermt@users.noreply.github.com>
2024-04-12 15:15:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "20919286"))
+API_HASH = environ.get("API_HASH", "57b85f72104db3f08f9795b0410eb556")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7101381082:AAGQQrFIDpGgSyi_voDJ-JoTv4hRIPmwRo0")
+ADMIN = int(environ.get("ADMIN", "5909932224"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
